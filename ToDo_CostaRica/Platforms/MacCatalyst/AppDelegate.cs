d2de8c2f98b633b09aceafebdbf406989c189e82
luqmanakram <Luqmanakram0030@gmail.com>
2024-12-19 01:42:33 +0500

    Update

---
+++ ToDo_CostaRica/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToDo_CostaRica;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
